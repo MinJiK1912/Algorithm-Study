cbe6c8a62a2ea59667764cba172e9b7711fcc104
MinJiK1912 <hibabel1219@gmail.com>
2021-08-27 15:29:56 +0900

    1. 로또의 최고 순위와 최저 순위

---
+++ 0827.js
@@ -0,0 +1,18 @@
+function solution(lottos, win_nums) {
+  let answer = [];
+  const LOTTO = [6, 6, 5, 4, 3, 2, 1]; //LOTTO[일치하는 갯수] = 순위
+  
+  //0이 몇개인지 count
+  //0을 제외한 숫자 중 일치하는 숫자가 몇개인지 count
+  //최고: 0 제외 일치 + 0 갯수 / 최저: 0 제외 일치
+  
+  let unknownNum = 0;
+  let lottoNum = 0;
+  
+  lottos.forEach((num) => {
+      if(num === 0) unknownNum++;
+      if(win_nums.includes(num)) lottoNum++;
+  })
+  
+  return [LOTTO[lottoNum + unknownNum], LOTTO[lottoNum]];
+}
