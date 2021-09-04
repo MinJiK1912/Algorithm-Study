14757eb754bc39f1731532f06e6d9c1b19b177f7
MinJiK1912 <hibabel1219@gmail.com>
2021-09-05 01:24:02 +0900

    9. 정수 제곱근 판별

---
+++ 0904.js
@@ -0,0 +1,11 @@
+//프로그래머스 1단계 문제
+//https://programmers.co.kr/learn/courses/30/lessons/12934
+
+function solution(n) {
+  const sqrt = parseInt(Math.sqrt(n));
+  
+  for(let i=1; i<=sqrt; i++) {
+      if(i*i === n) return Math.pow(i+1, 2);
+  }
+  return -1;
+}
