accafc13e961c442bb921300a7333f13e08fa997
MinJiK1912 <hibabel1219@gmail.com>
2021-09-05 01:27:25 +0900

    09/04 : 단계별 폴더로 분류

--- 0903.js
+++
@@ -1,9 +0,0 @@
-//프로그래머스 1단계 문제
-//https://programmers.co.kr/learn/courses/30/lessons/12947
-
-function solution(x) {
-  let sum = String(x).split('').reduce((sum, el) => sum+=Number(el), 0);
-  
-  if(x%sum === 0) return true;
-  else return false;
-}
